0d3216bfeb10b41d7eaa11292a3dd5badafa268d
p-k-pavan <pkpavan2003@email.com>
2025-06-09 08:27:56 +0530

    Mongo Db connection

--- server/server.js
+++ server/server.js
@@ -2,16 +2,25 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+dotenv.config();
 
-// Health check route
-app.get('/', (req, res) => {
-  res.send('✅ Backend is running!');
-});
 
-// Optional: API route to test
-app.get('/ping', (req, res) => {
-  res.json({ message: 'pong' });
-});
+mongoose
+  .connect(process.env.MONGODB, { serverSelectionTimeoutMS: 30000 })
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to DB:", error.message);
+
+    if (error.message.includes('timed out')) {
+      console.error('Connection attempt timed out. Please check your database server and network settings.');
+    } else {
+      console.error('An unexpected error occurred:', error);
+    }
+  });
 
 // Start server
 app.listen(PORT, () => {
